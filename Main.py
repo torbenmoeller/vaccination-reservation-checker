fe76b9f268d10dd2fb77c19e52913f3f5816b516
Torben Möller <torben.moeller@gmx.net>
2021-06-18 01:22:27 +0200

    feature: add scheduler for continuous checking

--- Main.py
+++ Main.py
@@ -1,17 +1,24 @@
-from datetime import time
+import sched
+import time
+
+from win10toast import ToastNotifier
 
 from BasePage import BasePage
 from NurembergPage import NurembergPage
 from Reservation import Reservation
 from SuendersbuehlPage import SuendersbuehlPage
-from win10toast import ToastNotifier
 
+s = sched.scheduler(time.time, time.sleep)
+ten_minutes = 60 * 10
+five_seconds = 5
+priority = 1
 base_url = "https://www.doctolib.de/medizinisches-versorgungszentrum-mvz/nuernberg/mvz-dr-renard-kollegen"
 suendersbuehl_url = base_url + "?pid=practice-159665"
 nuremberg_rul = base_url + "?pid=practice-159661"
 
 
 def process_page(key, base_page: BasePage):
+    print("Checking ", key)
     base_page.setup()
     result = base_page.test_availability()
     base_page.teardown()
@@ -24,11 +31,19 @@ def show_toast(key, result):
         toaster.show_toast(key, result.value)
 
 
-if __name__ == "__main__":
+def check_for_appointments():
+    print("Checking for appointments...")
     page_dic = {
         "Nuremberg": NurembergPage(nuremberg_rul),
         "Suendersbuehl": SuendersbuehlPage(suendersbuehl_url)
     }
     result_list = [process_page(key, page) for key, page in page_dic.items()]
+    print("Checking for appointments done")
     [print(key, ':', result.value) for key, result in result_list]
     [show_toast(key, result) for key, result in result_list]
+    s.enter(ten_minutes, priority, check_for_appointments)
+
+
+if __name__ == "__main__":
+    s.enter(five_seconds, priority, check_for_appointments)
+    s.run()
--- Reservation.py
+++ Reservation.py
@@ -1,4 +1,4 @@
-from enum import auto, Enum
+from enum import Enum
 
 
 class Reservation(Enum):
